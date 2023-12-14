af2aaea86a0c1d5c7742abead9d89ee89e044880
Eray Aynacı <73905858+er4y-c@users.noreply.github.com>
2023-12-14 04:56:23 +0300

    Add data connector

--- app/crud/example.py
+++ app/crud/example.py
@@ -1,7 +1,44 @@
+from fastapi import FastAPI, UploadFile, File
+import psycopg2
+import csv
+from dynaconf import settings
 from app.db.postgre import engine, Session
 from app.models.ExampleModel import HastaneVerileri
 
 session = Session(bind=engine)
 
-async def get_all_data():
+def get_all_data():
     return session.query(HastaneVerileri).all()
+
+def create_table(table_name, columns):
+    conn = psycopg2.connect(
+        database=settings.POSTGRE_DB_NAME,
+        user=settings.POSTGRE_DB_USER,
+        password=settings.POSTGRE_DB_PASSWORD,
+        host=settings.POSTGRE_DB_HOST,
+        port=settings.POSTGRE_DB_PORT
+    )
+    cur = conn.cursor()
+    # Tablo oluştur
+    query = f"CREATE TABLE IF NOT EXISTS {table_name} ({', '.join(columns)})"
+    cur.execute(query)
+    conn.commit()
+    conn.close()
+
+def insert_into_table(table_name, columns, data):
+    conn = psycopg2.connect(
+        database=settings.POSTGRE_DB_NAME,
+        user=settings.POSTGRE_DB_USER,
+        password=settings.POSTGRE_DB_PASSWORD,
+        host=settings.POSTGRE_DB_HOST,
+        port=settings.POSTGRE_DB_PORT
+    )
+    cur = conn.cursor()
+    # Verileri tabloya ekle
+    query = f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({', '.join(['%s'] * len(columns))})"
+    cur.executemany(query, data)
+    conn.commit()
+    conn.close()
+    
+def clean_column_names(columns):
+    return [col.replace(' ', '_') for col in columns]
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-from app.crud.example import get_all_data
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+from app.crud.example import get_all_data, create_table, insert_into_table, clean_column_names
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
@@ -13,7 +22,7 @@ async def root():
 @app.get("/get_data")
 async def get_data():
     try:
-        data = await get_all_data()
+        data = get_all_data()
         return JSONResponse(
             status_code=200,
             content={
@@ -25,5 +34,53 @@ async def get_data():
             status_code=500,
             content={
                 "message": f"An error occurred. Error details: {str(e)}"
+            }
+        )
+    
+@app.post("/data-connector/upload")
+async def upload_csv(file: UploadFile = File(...)):
+    if file.filename.endswith('.csv'):
+        table_name = file.filename.split('.')[0].lower()
+        table_name = table_name.replace(' ', '_').replace('-', '_').replace('.', '_').replace(',', '_')
+        contents = await file.read()
+        decoded = contents.decode('utf-8').splitlines()
+
+        columns = decoded[0].split(',')
+        columns = clean_column_names(columns)
+        create_table(table_name, [f"{col} VARCHAR(255)" for col in columns])
+
+        data = [row.split(',') for row in decoded[1:]]
+
+        insert_into_table(table_name, columns, data)
+
+        return JSONResponse(
+            status_code=200,
+            content={
+                "message": "File uploaded and data inserted into the database."
+            }
+        )
+
+    elif file.filename.endswith('.xlsx'):
+        table_name = file.filename.split('.')[0].lower()
+        table_name = table_name.replace(' ', '_').replace('-', '_').replace('.', '_').replace(',', '_')
+        contents = await file.read()
+        df = pd.read_excel(contents)
+        columns = df.columns.tolist()
+        create_table(table_name, [f"{col.lower().replace(' ', '_')} VARCHAR(255)" for col in columns])
+        data = df.values.tolist()
+
+        insert_into_table(table_name, columns, data)
+
+        return JSONResponse(
+            status_code=200,
+            content={
+                "message": "File uploaded and data inserted into the database."
+            }
+        )
+    else:
+        return JSONResponse(
+            status_code=400,
+            content={
+                "message": "File type not supported. Please upload a CSV or Excel file."
             }
         )
